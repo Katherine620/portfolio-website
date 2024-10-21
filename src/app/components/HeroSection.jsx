0cde8454bf48a2ba87713ba33548d0a4c2ab7f7b
Katherine Lee <katherinelee@Katherines-MacBook-Pro.local>
2024-10-21 15:19:56 -0500

    feat: add hero section component

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+    return (
+        <section>
+            <h1 className="text-white">Helol, I'm Katherine</h1>
+        </section>
+    );
+};
+
+export default HeroSection;
