ee0fd61fbb54725786f3dfc045ae2b39cb7d17c9
Katherine Lee <katherinelee@Katherines-MacBook-Pro.local>
2024-11-11 15:18:06 -0600

    feat: update route and email section component

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,17 +2,23 @@
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
+export async function POST(req, res) {
+  const { body } = await req.json();
+  const { email, subject, message } = body;
+
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Katherine <katherinelicode@gmail.com>',
-      to: ['katherinelicode@gmail.com'],
-      subject: 'Hello world',
+      from: fromEmail,
+      to: ["katherinelicode@gmail.com", email],
+      subject: subject,
     //   react: EmailTemplate({ firstName: 'John' }),
       react: (
         <>
-            <p>Email Body</p>
+            <h1>{subject}</h1>
+            <p>Thank you for contacting us!</p>
+            <p>{message}</p>
         </>
       ),
     });
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import GithubIcon from '../../../public/github-icon.svg';
 import LinkedinIcon from '../../../public/linkedin-icon.svg';
 import Link from 'next/link';
@@ -6,6 +7,33 @@ import Image from 'next/image';
 import { SocialURLs } from '../constants/constants';
 
 const EmailSection = () => {
+    const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: e.target.email.value,
+            subject: e.target.subject.value,
+            message: e.target.message.value,
+        }
+        const JSONdata = JSON.stringify(data);
+        const endpoint = "/api/send";
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSONdata,
+        }
+
+        const response = await fetch(endpoint, options);
+        const resData = await response.json();
+        if (response.status === 200) {
+            console.log('Message sent');
+            setEmailSubmitted(true);
+        }
+    };
     return (
         <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
             <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-fuchsia-800 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
@@ -27,12 +55,13 @@ const EmailSection = () => {
                 </div>
             </div>
             <div>
-                <form className="flex flex-col">
+                <form className="flex flex-col" onSubmit={handleSubmit}>
                     <div className="mb-6">
                         <label htmlFor="email" className="text-white block mb-2 text-sm font-medium">
                             Your email
                         </label>
                         <input 
+                            name="email"
                             type="email" 
                             id="email" 
                             required
@@ -45,6 +74,7 @@ const EmailSection = () => {
                             Subject
                         </label>
                         <input 
+                            name="subject"
                             type="text" 
                             id="subject" 
                             required
@@ -57,6 +87,7 @@ const EmailSection = () => {
                             Message
                         </label>
                         <textarea 
+                            name="message"
                             type="message" 
                             id="message" 
                             required
@@ -70,6 +101,13 @@ const EmailSection = () => {
                     >
                         Send Message
                     </button>
+                    {
+                        emailSubmitted && (
+                            <p className="text-green-500 text-sm mt-2">
+                                Email sent successfully!
+                            </p>
+                        )
+                    }
                 </form>
             </div>
         </section>
