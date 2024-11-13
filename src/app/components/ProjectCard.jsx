c6b4fbb0a375f73f5785c78dfb5be6a7b717eebd
Katherine Lee <katherinelee@Katherines-MacBook-Pro.local>
2024-11-13 16:52:50 -0600

    chore: hide unused buttons for project card component

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -10,26 +10,29 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
                 style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}
             >
                 <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-                    <Link 
-                        href={gitUrl}
-                        className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#adb7be] hover:border-white group/link"
-                        {...(gitUrl !== "/" ? { 
-                            target: "_blank", 
-                            rel: "noopener noreferrer" 
-                        } : {})}
-                    >
-                        <CodeBracketIcon className="h-10 w-10 text-[#adb7be] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
-                    </Link>
-                    <Link 
-                        href={previewUrl} 
-                        className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#adb7be] hover:border-white group/link"
-                        {...(previewUrl !== "/" ? { 
-                            target: "_blank", 
-                            rel: "noopener noreferrer" 
-                        } : {})}
-                    >
-                        <EyeIcon className="h-10 w-10 text-[#adb7be] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
-                    </Link>
+                    { gitUrl !== "/" && (
+                        <Link 
+                            href={gitUrl}
+                            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#adb7be] hover:border-white group/link"
+                            target="_blank"
+                            rel="noopener noreferrer" 
+                        >
+                            <CodeBracketIcon className="h-10 w-10 text-[#adb7be] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+                        </Link>
+                    )}
+                    { previewUrl !== "/" && (
+                        <Link 
+                            href={previewUrl} 
+                            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#adb7be] hover:border-white group/link"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <EyeIcon className="h-10 w-10 text-[#adb7be] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+                        </Link>
+                    )}
+                    { gitUrl === "/" && previewUrl === "/" && (
+                        <p className="text-[#adb7be] font-semibold text-xl">Coming Soon...</p>
+                    )}
                 </div>
             </div>
             <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
