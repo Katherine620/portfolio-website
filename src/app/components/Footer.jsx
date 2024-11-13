715f8b72ae33f463452a02960e2eed605f211c9c
Katherine Lee <katherinelee@Katherines-MacBook-Pro.local>
2024-11-12 20:14:51 -0600

    chore: adjust footer position

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from "../../../public/images/logo.svg";
 
 const Footer = () => {
     return (
-        <footer className="footer border border-t-[#33353f] border-l-transparent border-r-transparent text-white">
+        <footer className="footer border z-10 border-t-[#33353f] border-l-transparent border-r-transparent text-white">
             <div className="container p-12 flex justify-between">
                 <span><Image src={Logo} alt="logo" className="h-[50px]" /></span>
                 <p className="text-slate-600 text-base font-medium">Copyright 2024. All rights reserved.</p>
