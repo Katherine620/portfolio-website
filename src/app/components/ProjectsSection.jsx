4409176e2bdf3d2c12e5bb5dfeae642d0b4c5f1a
Katherine Lee <katherinelee@Katherines-MacBook-Pro.local>
2024-11-13 20:12:00 -0600

    feat: add animation for project section

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
 import { ProjectURLs } from "../constants/constants";
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
     {
@@ -63,6 +64,8 @@ const projectsData = [
 
 const ProjectsSection = () => {
     const [tag, setTag] = useState("All");
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
 
     const handleTagChange = (newTag) => {
         setTag(newTag);
@@ -71,6 +74,11 @@ const ProjectsSection = () => {
     const filteredProjects = projectsData.filter((project) => 
         project.tag.includes(tag)
     );
+
+    const cardVariants = {
+        initial: { y: 50, opacity: 0 },
+        animate: { y: 0, opacity: 1 },
+    };
 
     return (
         <section id="projects">
@@ -94,18 +102,26 @@ const ProjectsSection = () => {
                     isSelected={tag === "Mobile"} 
                 />
             </div>
-            <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-                {filteredProjects.map((project) => (
-                    <ProjectCard
-                        key={project.id} 
-                        title={project.title} 
-                        description={project.description} 
-                        imgUrl={project.image} 
-                        gitUrl={project.gitUrl}
-                        previewUrl={project.previewUrl}
-                    />
+            <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+                {filteredProjects.map((project, index) => (
+                    <motion.li 
+                        key={index}
+                        variants={cardVariants} 
+                        initial="initial" 
+                        animate={isInView ? "animate": "initial"}
+                        transition={{ duration: 0.3, delay: index * 0.4 }}
+                    >
+                        <ProjectCard
+                            key={project.id} 
+                            title={project.title} 
+                            description={project.description} 
+                            imgUrl={project.image} 
+                            gitUrl={project.gitUrl}
+                            previewUrl={project.previewUrl}
+                        />
+                    </motion.li>
                 ))}
-            </div>
+            </ul>
         </section>
     )
 };
