fc3fe85bbca2802137ebe4f81a5ce309ff15ffe0
Katherine Lee <katherinelee@Katherines-MacBook-Pro.local>
2024-11-14 16:14:43 -0600

    feat: update connect button animation

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -10,8 +10,8 @@ const EmailSection = () => {
     return (
         <section className="grid md:grid-rows-2 my-12 md:my-12 py-24 gap-4 relative justify-items-center" id="contact">
             <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-tertiary-800 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-            <div className="z-10 justify-items-center">
-                <h1 className="text-2xl md:text-4xl font-bold text-white my-2">Let&apos;s Connect</h1>
+            <div className="z-10 justify-items-center -mt-8">
+                <h1 className="text-3xl md:text-4xl font-bold text-white my-4 md:my-2">Let&apos;s Connect</h1>
                 <p className="text-[#adb7be] mb-4 max-w-md md:text-center">
                     {" "}
                     I&apos;m currently looking for new opportunities, my inbox is always open. 
@@ -27,10 +27,10 @@ const EmailSection = () => {
                     </Link>
                 </div>
             </div>
-            <div className="grid place-items-center">
+            <div className="grid place-items-center -mb-12">
                 <a
                     href={EmailInfo.MAIL_TO}
-                    className="bg-tertiary-500 hover:bg-tertiary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full transitaion-all duration-300 ease-in-out hover:-translate-x-2 hover:-translate-y-2 hover:shadow-lg hover:shadow-tertiary-500/80"
+                    className="relative top-0 left-0 bg-tertiary-500 hover:bg-tertiary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full transitaion-all duration-300 ease-in-out hover:-translate-x-2 hover:-translate-y-2 hover:shadow-[8px_8px_0_rgba(217,70,239,0.4)]"
                 >
                     Say Hello
                 </a>
