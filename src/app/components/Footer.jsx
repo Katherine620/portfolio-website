cb00a4fd0463b03b102d44a52a3a770a4254043c
Katherine Lee <katherinelee@Katherines-MacBook-Pro.local>
2024-11-14 15:53:21 -0600

    feat: update email section

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ import Logo from "../../../public/images/logo.svg";
 const Footer = () => {
     return (
         <footer className="footer border z-10 border-t-[#33353f] border-l-transparent border-r-transparent text-white">
-            <div className="container p-12 flex justify-between">
+            <div className="container p-12 flex justify-between place-items-center">
                 <span><Image src={Logo} alt="logo" className="h-[50px]" /></span>
                 <p className="text-slate-600 text-base font-medium">Copyright 2024. All rights reserved.</p>
             </div>
